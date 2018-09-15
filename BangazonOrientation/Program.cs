272c6d9eb2509bbff0b9f455ac910213e8bc3986
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-15 09:32:31 -0500

    Add project files.

---
+++ BangazonOrientation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BangazonOrientation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
